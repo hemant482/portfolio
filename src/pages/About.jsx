a593469736848fb65c04a8bfa0bb98707f9dfefa
Hemant Kumawat <hemantkumawat482@gmail.com>
2025-04-19 15:36:17 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
 
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-            <h2 className="text-3xl font-bold text-gray-900 mb-8">About Me</h2>
+            <h2 className="text-3xl text-center font-bold text-gray-900 mb-8">About Me</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
                 <div>
