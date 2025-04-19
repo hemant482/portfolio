0f25a6651f2c970a2904cc8d29de33d349c49cb2
Hemant Kumawat <hemantkumawat482@gmail.com>
2025-04-19 15:36:37 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-      <h2 className="text-3xl font-bold text-gray-900 mb-8">My Projects</h2>
+      <h2 className="text-3xl text-center font-bold text-gray-900 mb-8">My Projects</h2>
       
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
         {projects.map((project, index) => (
