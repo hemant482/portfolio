8cd3c13db2bb78e9c99ba506f695215a174d31bd
Hemant Kumawat <hemantkumawat482@gmail.com>
2025-04-19 15:32:32 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,59 +1,84 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-  const skills = [
-    'HTML', 'CSS', 'JavaScript', 'Python', 'React', 'Bootstrap',  'SQL', 'PHP', 'SEO', 'Git'
-  ];
+    const skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Python",
+        "React",
+        "Bootstrap",
+        "SQL",
+        "PHP",
+        "SEO",
+        "Git",
+    ];
 
-  return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
-      <h2 className="text-3xl font-bold text-gray-900 mb-8">About Me</h2>
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-        <div>
-          <h3 className="text-2xl font-semibold text-navy-600 mb-4">Education</h3>
-          <div className="space-y-6">
-            <div>
-              <h4 className="text-xl font-medium">Master of Computer Application</h4>
-              <p className="text-gray-700">Mohanlal Sukhadia University, Udaipur</p>
-              <p className="text-gray-600">2023 - Present • 7.5 CGPA</p>
-            </div>
-            <div>
-              <h4 className="text-xl font-medium">Bachelor of Computer Application</h4>
-              <p className="text-gray-700">
-              Pandit Deendayal Upadhyaya Shekhawati University</p>
-              <p className="text-gray-600">2020 - 2023 • 74%</p>
-            </div>
-            <div>
-              <h4 className="text-xl font-medium">12th Standard</h4>
-              <p className="text-gray-700">RBSE</p>
-              <p className="text-gray-600">2020 • 71%</p>
-            </div>
-            <div>
-              <h4 className="text-xl font-medium">10th Standard</h4>
-              <p className="text-gray-700">RBSE</p>
-              <p className="text-gray-600">2018 • 70%</p>
-            </div>
-            
-          </div>
-        </div>
+    return (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+            <h2 className="text-3xl font-bold text-gray-900 mb-8">About Me</h2>
 
-        <div>
-          <h3 className="text-2xl font-semibold text-navy-600 mb-4">Skills</h3>
-          <div className="grid grid-cols-2 gap-4">
-            {skills.map((skill, index) => (
-              <div
-                key={index}
-                className="bg-white shadow rounded-lg p-5 text-center"
-              >
-                {skill}
-              </div>
-            ))}
-          </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                <div>
+                    <h3 className="text-2xl font-semibold text-navy-600 mb-4">
+                        Education
+                    </h3>
+                    <div className="space-y-6">
+                        <div>
+                            <h4 className="text-xl font-medium">
+                                Master of Computer Applications (MCA)
+                            </h4>
+                            <p className="text-gray-700">
+                                Mohanlal Sukhadia University, Udaipur
+                            </p>
+                            <p className="text-gray-600">
+                                2023 - Present • 7.3 CGPA
+                            </p>
+                        </div>
+                        <div>
+                            <h4 className="text-xl font-medium">
+                                Bachelor of Computer Applications (BCA)
+                            </h4>
+                            <p className="text-gray-700">
+                                Balaji PG College, Sikar
+                            </p>
+                            <p className="text-gray-600">2020 - 2023 • 74%</p>
+                        </div>
+                        <div>
+                            <h4 className="text-xl font-medium">
+                                12th Standard (RBSE) - PCB
+                            </h4>
+                            <p className="text-gray-700">MK. Memorial School, Sikar</p>
+                            <p className="text-gray-600">2020 • 71%</p>
+                        </div>
+                        <div>
+                            <h4 className="text-xl font-medium">
+                                10th Standard (RBSE)
+                            </h4>
+                            <p className="text-gray-700">Saint Paul's School, Sikar</p>
+                            <p className="text-gray-600">2018 • 70%</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <h3 className="text-2xl font-semibold text-navy-600 mb-4">
+                        Skills
+                    </h3>
+                    <div className="grid grid-cols-2 gap-4">
+                        {skills.map((skill, index) => (
+                            <div
+                                key={index}
+                                className="bg-white shadow rounded-lg p-5 text-center"
+                            >
+                                {skill}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default About;
